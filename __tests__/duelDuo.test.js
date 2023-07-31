a01ffb27fa6a704fbb7d987ba04a16cf2ff4263c
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-07-30 17:45:45 -0700

    fourth

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -12,15 +12,15 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:3000/");
+    await driver.get("http://3.141.9.190/");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
   test("Find the draw button", async()=>{
-    await driver.get("http://localhost:3000/");
+    await driver.get("http://3.141.9.190/");
     await driver.wait(until.elementLocated(By.id("draw")), 1000);
   });
   test("Check that clicking the Draw button displays the div id='choices'.", async ()=>{
-    await driver.get("http://localhost:3000/");
+    await driver.get("http://3.141.9.190/");
     await driver.wait(until.elementLocated(By.id("draw")), 1000);
     await driver.findElement(By.id("draw")).click();
     await driver.wait(until.elementLocated(By.id("choices")),1000)
